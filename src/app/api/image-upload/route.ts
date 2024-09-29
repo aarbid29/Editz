e3fb9d990a15e12bd38b3f8745cee8121ee083bb
aarbid29 <aarbid.b@gmail.com>
2024-09-29 18:47:49 +0545

    changes

--- src/app/api/image-upload/route.ts
+++ src/app/api/image-upload/route.ts
@@ -1,11 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
 import { v2 as cloudinary } from "cloudinary";
-import { NextResponse, NextRequest } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 
+// Configuration
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
 });
 
 interface CloudinaryUploadResult {
@@ -17,35 +18,53 @@ export async function POST(request: NextRequest) {
   const { userId } = auth();
 
   if (!userId) {
-    return NextResponse.json({ err: "unauthorized" }, { status: 401 });
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   try {
+    // console.log(process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME);
     const formData = await request.formData();
-    const file = formData.get("file") as File;
+    console.log(formData);
+    const file = formData.get("file");
 
-    if (!file) {
-      return NextResponse.json({ err: "no file" }, { status: 400 });
+    // Check if the file is valid
+    if (!file || !(file instanceof File)) {
+      return NextResponse.json(
+        { error: "File not found or invalid" },
+        { status: 400 }
+      );
     }
-    //file's property arraybuffer to have  bytes to create buffer
+
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
-    ////
+
     const result = await new Promise<CloudinaryUploadResult>(
       (resolve, reject) => {
         const uploadStream = cloudinary.uploader.upload_stream(
-          { folder: "cloudinart_upload1" },
+          { folder: "next-cloudinary-uploads" },
           (error: any, result: any) => {
-            if (error) reject(error);
-            else resolve(result as CloudinaryUploadResult);
+            if (error) {
+              console.error("Cloudinary upload error:", error);
+              reject(error);
+            } else {
+              resolve(result as CloudinaryUploadResult);
+            }
           }
         );
         uploadStream.end(buffer);
       }
     );
 
-    return NextResponse.json({ publicId: result.publicId }, { status: 200 });
-  } catch (err: any) {
-    return NextResponse.json({ err: "image upload failed" }, { status: 500 });
+    return NextResponse.json(
+      {
+        publicId: result.public_id,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error("Upload image failed:", error);
+    return NextResponse.json({ error: "Upload image failed" }, { status: 500 });
   }
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,47 +1,43 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/sign-in",
   "/sign-up",
-  "/home",
   "/",
+  "/home",
+  // "/size-edit",
 ]);
-
-const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
+const isPublicApiRoute = createRouteMatcher([
+  "/api/videos",
+  "/api/image-upload",
+]);
 
 export default clerkMiddleware((auth: any, req: any) => {
   const { userId } = auth();
   const currentUrl = new URL(req.url);
-
-  const isHome = currentUrl.pathname === "/home";
+  const isAccessingDashboard = currentUrl.pathname === "/home";
   const isApiRequest = currentUrl.pathname.startsWith("/api");
 
-  // If the user is logged in and accessing a public route (but not home) go  to home
-  if (userId && isPublicRoute(req) && !isHome) {
+  // If user is logged in and accessing a public route but not the dashboard
+  if (userId && isPublicRoute(req) && !isAccessingDashboard) {
     return NextResponse.redirect(new URL("/home", req.url));
   }
-
-  // If the user is not logged in
+  //not logged in
   if (!userId) {
-    // If the request is for a protected route
-    if (!isPublicRoute(req)) {
+    // If user is not logged in and trying to access a protected route
+    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
       return NextResponse.redirect(new URL("/sign-in", req.url));
     }
 
-    // If the request is for a protected API route
+    // If the request is for a protected API and the user is not logged in
     if (isApiRequest && !isPublicApiRoute(req)) {
       return NextResponse.redirect(new URL("/sign-in", req.url));
     }
   }
-
   return NextResponse.next();
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?j|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
